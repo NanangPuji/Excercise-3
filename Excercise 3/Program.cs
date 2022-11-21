0d054271c19bb2774ff000a6ab38d5850d6f5433
Nanang Puji <nanangpuji762@gmail.com>
2022-11-21 15:59:05 +0700

    Add project files.

---
+++ Excercise 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Excercise_3
+{
+    internal class Program
+    {
+        class Node
+        {
+            /*creates Node for the cicular nexted list*/
+            public int rollNumber;
+            public string nama;
+            public Node next;
+        }
+        class CircularList
+        {
+            Node LAST;
+            public CircularList()
+            {
+                LAST = null;
+            }
+
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
